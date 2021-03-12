7e0af535e57aa2097449d80b09e943e3d6f2c813
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-12 15:27:58 -0300

    style: create globalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+   *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+   }
+
+   html{
+    max-width: 100vw;
+    max-height: 100vh;
+
+    width: 100%;
+    height: 100%;
+    }
+
+   :root{
+       --dark-background-color: #312e2b;
+       --dark-text-color: hsla(0,0%,100%,.65);
+       --dark-text-color-hover: #fff;
+       --dark-background-color-aside: #292725;
+   }
+`;
