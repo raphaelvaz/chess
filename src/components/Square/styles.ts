e041f1b120056410972964225567d0d8dd2a0b0a
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-21 17:08:40 -0300

    refactor: change method to select piece the color of pieces and squares

--- src/components/Square/styles.ts
+++ src/components/Square/styles.ts
@@ -1,19 +1,31 @@
 import styled from 'styled-components';
 
-interface ContainerProps {
-    row: number;
-    col: number;
+interface ButtonProps {
+    player: number
+    index: number
 }
 
-export const Container = styled.button<ContainerProps>`
+export const Container = styled.button<ButtonProps>`
     display:flex;
+    align-items: center;
+    justify-content:center;
+
     min-width: 40px;
     min-height: 40px;
 
     width: min(8vw, min(10vh, 70px));
     height: min(8vw, min(10vh, 70px));
+    
+    border: 1px black solid;
 
-background-color: ${props => props.row % 2 ? props.col % 2 ? '#64a864' : '#fff' : props.col % 2 ? '#fff' : '#64a864'};
+    background-color: ${props => ((
+        props.index > 7 && props.index < 16)
+        || (props.index > 23 && props.index < 32)
+        || (props.index > 39 && props.index < 48)
+        || (props.index > 55 && props.index < 64)
+        ? !(props.index % 2) ? '#a0522d' : '#c9bd4e' : !(props.index % 2) ? '#c9bd4e' : '#a0522d')};
 
-border:none;
+    svg{
+        color:${props => props.player === 1 ? '#fff' : '#000'};
+    }
 `;
