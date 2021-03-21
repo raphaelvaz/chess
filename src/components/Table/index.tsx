81cc5b8c19eeb7c34c82fcd27a548686b5a1e008
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-21 17:09:55 -0300

    refactor: add a handleClick function to Square component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Square from '../Square'
 import { Container } from './styles';
+import initChess from '../helpers/initChess'
+
+interface PieceProps {
+    player: number,
+    type: string,
+}
 
 const Table: React.FC = () => {
-    const grid = Array(8).fill(0).map(x => Array(8).fill(''));
+    const [source, setSource] = useState(-1);
+    const [table, setTable] = useState(initChess())
+
+    function handleClick(dest: number, square: PieceProps) {
+        if (!square.type && source === -1) {
+            console.log('escolha peças validas para jogar')
+            return;
+        } else if (square.type) {
+            setSource(dest)
+            console.log('escolha um destino')
+            return;
+        }
+        if (source > 0) {
+            console.log('peça movida!')
+            console.log(source, source)
+            const newTable = table.map((content, index) => {
+                if (source === index) {
+                    return {}
+                }
+                if (dest === index) {
+                    return {
+                        player: 1,
+                        type: 'pawn',
+                    }
+                }
+                return content
+            })
+            console.log(newTable)
+            setTable([...newTable])
+            setSource(-1)
+        }
+    }
+
+
     return (
         <Container>
-            {grid.map((row, i) => {
-                return (
-                    row.map((positon, j) => {
-                        return (<Square key={i + '-' + j} row={j} col={i}></Square>)
-                    }))
-            })}
+            {table.map(((square: PieceProps, index: number) => {
+                return <Square index={index} key={index} content={square} onClick={() => handleClick(index, square)} />
+            }))}
         </Container>
     )
 }
