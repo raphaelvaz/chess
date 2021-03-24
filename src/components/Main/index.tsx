295552237e7b126e511e28baba0fbb70e393215a
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-24 19:27:08 -0300

    refactor: ensure MainPage show users names

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import Table from '../Table'
 import { Container, Profile, ProfileText } from './styles';
+import { useUsers } from '../../hooks/user'
 
 const Main: React.FC = () => {
+    const { users } = useUsers()
     return (
 
         <Container>
             <Profile>
                 <img src='https://avatars.githubusercontent.com/u/42014582?s=460&u=2d7def55f1d78438417f4c6513a9b1dbea6bb149&v=4' alt='profileImage' />
                 <ProfileText>
-                    <strong>raphaeltrombetta</strong><span>(1000)</span>
+                    <strong>{users[0].name}</strong><span>(1000)</span>
                 </ProfileText>
                 {/* <Timer /> */}
             </Profile>
@@ -18,7 +20,7 @@ const Main: React.FC = () => {
             <Profile>
                 <img src='https://avatars.githubusercontent.com/u/42014582?s=460&u=2d7def55f1d78438417f4c6513a9b1dbea6bb149&v=4' alt='profileImage' />
                 <ProfileText>
-                    <strong>miro</strong><span>(1200)</span>
+                    <strong>{users[1].name}</strong><span>(1200)</span>
                 </ProfileText>
                 {/* <Timer /> */}
             </Profile>
