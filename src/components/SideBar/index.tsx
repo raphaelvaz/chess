62969b90cdfa43190daec132f5cc204f8ce57ab5
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-24 21:15:49 -0300

    refactor: remove tabs from sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,41 +1,31 @@
 import React from 'react';
-import {
-    BsClockFill, BsPlusSquareFill, FaChessBoard, BsPeopleFill,
-    BsFlagFill, FaEquals, BiArrowToRight, BiArrowToLeft,
-    MdKeyboardArrowLeft, MdKeyboardArrowRight, BsPlayFill,
-    ImLoop, BsGearFill
-} from '../../styles/icons'
-import { Container, Tabs, Tab, Plays, TaskBar } from './styles'
+import { FaEquals, BsFlagFill } from '../../styles/icons'
+import { Container, Plays, TaskBar } from './styles'
 import Chat from '../Chat/index'
+
 
 
 const SideBar: React.FC = () => {
     return (
         <Container>
-            <Tabs >
+            {/* <Tabs >
                 <Tab ><BsClockFill size={25} /><span>Partida</span></Tab>
                 <Tab ><BsPlusSquareFill size={25} />Nova partida</Tab>
                 <Tab ><FaChessBoard size={25} /> Partidas</Tab>
                 <Tab ><BsPeopleFill size={25} /> Jogadores</Tab>
             </Tabs>
+            <Plays /> */}
             <Plays />
             <TaskBar >
-                <div>
+                <button>
                     <FaEquals size={25} />
                     <strong>Empate</strong>
-                </div>
+                </button>
 
-                <div>
+                <button>
                     <BsFlagFill size={25} />
                     <strong>Desistir</strong>
-                </div>
-                <BiArrowToLeft size={24} />
-                <MdKeyboardArrowLeft size={25} />
-                <MdKeyboardArrowRight size={25} />
-                <BiArrowToRight size={24} />
-                <BsPlayFill size={24} />
-                <ImLoop size={20} />
-                <BsGearFill size={20} />
+                </button>
             </TaskBar>
             <Chat />
         </Container >
--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -4,12 +4,12 @@ import styled from 'styled-components'
 export const Container = styled.div`
     min-width: 400px;
     width: min(50vw, 500px);
-
-    margin: auto 0;
+    margin-bottom: 20px;
     border-radius: 5px;
 
     background-color: var(--dark-background-color-aside);
     color: var(--dark-text-color);
+    
 `;
 export const Tabs = styled.div`
     width: 100%;
@@ -47,7 +47,7 @@ export const Tab = styled.div`
 
 export const Plays = styled.div`
     width: 100%;
-    height: 35vh;
+    height: 110px;
     background-color:var(--dark-background-color-aside);
 
     @media (max-width: 970px){
@@ -56,22 +56,44 @@ export const Plays = styled.div`
 `;
 
 export const TaskBar = styled.div`
-    width: 100%;
-    height: 50px;
+    width: 97%;
+    height: 40px;
+    margin: auto;
 
     display: flex;
     align-items: center;
     justify-content: space-around;
 
-    background-color: var(--dark-background-color-sidebar);
+    background-color: var(--dark-background-color-aside);
 
-    div{
+    button{
+        width: 100%;
+        height: 35px;
         display:flex;
         align-items:center;
         justify-content:center;
 
+        border-radius: 5px;
+        border:none;
+
+        margin-right: 5px;
+
+        font-weight: bold;
+
         svg{
             margin-right: 3px;
+        }
+
+        background-color: var(--dark-background-color-aside-hover);
+        color: var(--dark-text-color);
+
+        outline: none;
+        cursor: pointer;
+
+        transition: background-color 0.2s;
+
+        &:hover{
+            background-color: #1b1919;
         }
     }
 `;
