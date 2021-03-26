539d3362deaf257a5789d0034f596ec9db1f4a7e
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-25 21:36:28 -0300

    refactor: change toast properties and add a timer

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Title, Content } from './styles';
 import { BiInfoCircle } from '../../styles/icons'
+import { ToastMessage, useToast } from '../../hooks/toast'
 
 interface ToastProps {
-    title: string;
-    content: string;
+    message: ToastMessage
 }
 
-const Toast: React.FC<ToastProps> = ({ content, title }) => {
+const Toast: React.FC<ToastProps> = ({ message }) => {
+    const { removeToast } = useToast()
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            removeToast()
+        }, 5000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [removeToast])
+
     return (
         <Container>
             <div>
                 <BiInfoCircle size={25} />
-                <Title>{title}</Title>
+                <Title>{message.title}</Title>
             </div>
-            <Content>{content}</Content>
+            <Content>{message.content}</Content>
         </Container>);
 }
 
--- src/components/Toast/styles.ts
+++ src/components/Toast/styles.ts
@@ -12,19 +12,15 @@ export const Container = styled.div`
 
     width: 350px;
 
-    position: relative;
-    bottom: 100px;
-
-    @media (max-width: 710px){
-        position: relative;
-        bottom: 50px;
-    }
+    position: absolute;
+    right:10px;
+    top:10px;
+    padding: 10px;
+    overflow: hidden;
 
     border-radius: 10px;
     background-color: #fdbc45;
     color: #fff;
-
-    padding: 10px;
 `;
 
 export const Title = styled.strong`
